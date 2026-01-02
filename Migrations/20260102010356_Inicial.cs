e495920866e8302722342650cc3feb74231c0aa7
Joao Dumba <72528227+tioya2923@users.noreply.github.com>
2026-01-02 01:11:44 +0000

    Adiciona migrations iniciais

---
+++ Migrations/20260102010356_Inicial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MissaoBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
---
+++ Migrations/20260102010944_NomeDaMudacaTopico.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MissaoBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class NomeDaMudacaTopico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
